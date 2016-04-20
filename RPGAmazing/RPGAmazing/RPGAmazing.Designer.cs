4dd6362abf752d89c7bb0dd7f3e2b3505755b158
geoffsim <geoffsim95@gmail.com>
2016-04-20 17:03:10 -0400

    Need to make all buttons function still, but working

--- RPGAmazing/RPGAmazing/RPGAmazing.Designer.cs
+++ RPGAmazing/RPGAmazing/RPGAmazing.Designer.cs
@@ -176,6 +176,7 @@
             this.btnNorth.TabIndex = 13;
             this.btnNorth.Text = "North";
             this.btnNorth.UseVisualStyleBackColor = true;
+            this.btnNorth.Click += new System.EventHandler(this.btnNorth_Click);
             // 
             // btnEast
             // 
